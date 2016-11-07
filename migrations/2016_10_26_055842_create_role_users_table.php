aa462b5fa810d2d8c397d72e66564b9b0f071db3
Shin Zehir <zehirin@gmail.com>
2016-11-07 17:37:12 +0800

    fix role_users table

--- migrations/2016_10_26_055842_create_role_users_table.php
+++ migrations/2016_10_26_055842_create_role_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateRoleUserTable extends Migration
+class CreateRoleUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateRoleUserTable extends Migration
      */
     public function up()
     {
-        Schema::create('role_user', function (Blueprint $table) {
+        Schema::create('role_users', function (Blueprint $table) {
             $table->integer('user_id');
             $table->string('role_id');
             $table->primary(['user_id', 'role_id']);
@@ -28,6 +28,6 @@ class CreateRoleUserTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('role_user');
+        Schema::dropIfExists('role_users');
     }
 }
